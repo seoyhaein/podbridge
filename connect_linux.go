591da3afceed4e912170c7265b725b50b105a954
seoyhaein <seoy@bio-caleb.com>
2022-05-24 18:59:49 +0900

    connect_linux.go 추가

--- connect_linux.go
+++ connect_linux.go
@@ -2,24 +2,20 @@ package podbridge
 
 import (
 	"context"
-	"log"
 	"os"
 
 	"github.com/containers/podman/v4/pkg/bindings"
 )
 
-func GetConnection() *context.Context {
+func GetConnection() (*context.Context, error) {
 	sockDir := os.Getenv("XDG_RUNTIME_DIR")
 
 	if sockDir == "" {
 		sockDir = "/var/run"
 	}
+
 	socket := "unix:" + sockDir + "/podman/podman.sock"
-
 	connText, err := bindings.NewConnection(context.Background(), socket)
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	return &connText
+	return &connText, err
 }
