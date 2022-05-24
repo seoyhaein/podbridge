46795355d04234fe19f3b1bc7f21dc425f855187
seoyhaein <seoy@bio-caleb.com>
2022-05-24 19:11:21 +0900

    connect_linux.go 추가

--- connect_linux.go
+++ connect_linux.go
@@ -7,7 +7,15 @@ import (
 	"github.com/containers/podman/v4/pkg/bindings"
 )
 
-func GetConnection() (*context.Context, error) {
+// return 값이 *context.Context 이라는 것을 주의하자.
+// ipc 로 restful 연결 임으로 지속적으로 연결을 유지할 필요가 없지 않을까?
+
+func GetConnection(ctx context.Context) (*context.Context, error) {
+	// 주석처리된 코드는 필요없지만 일단은 주석 처리된 상태로 넣어 놓았다.
+	/*
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+	*/
 	sockDir := os.Getenv("XDG_RUNTIME_DIR")
 
 	if sockDir == "" {
@@ -15,7 +23,7 @@ func GetConnection() (*context.Context, error) {
 	}
 
 	socket := "unix:" + sockDir + "/podman/podman.sock"
-	connText, err := bindings.NewConnection(context.Background(), socket)
+	connText, err := bindings.NewConnection(ctx, socket)
 
 	return &connText, err
 }
