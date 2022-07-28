8a935ec84ae756479ad61a2620e0339aed8ae1cf
seoyhaein <seoy@bio-caleb.com>
2022-07-28 20:58:17 +0900

    doc commit

--- podman_install.sh
+++ podman_install.sh
@@ -2,9 +2,42 @@
 
 set -ex
 
+# install dependency
+# Debian, Ubuntu, and related distributions: -> https://podman.io/getting-started/installation
+sudo apt-get install \
+  gcc \
+  btrfs-progs \
+  go-md2man \
+  iptables \
+  libassuan-dev \
+  libbtrfs-dev \
+  libc6-dev \
+  libdevmapper-dev \
+  libglib2.0-dev \
+  libgpgme-dev \
+  libgpg-error-dev \
+  libprotobuf-dev \
+  libprotobuf-c-dev \
+  libseccomp-dev \
+  libselinux1-dev \
+  libsystemd-dev \
+  pkg-config \
+  uidmap
+
+# install runc
+git clone -b v1.1.3 https://github.com/opencontainers/runc.git && cd runc
+sudo copy runc /usr/local/bin
+make
+sudo make install
+
+# install conmon
+git clone -b v2.1.3 https://github.com/containers/conmon.git && cd conmon
+make
+
+# install podman
 git clone -b v4.1.0 https://github.com/containers/podman.git && cd podman
-make binaries
+sudo make binaries
 cd bin/
-copy podman /usr/local/bin
-systemctl --user enable --now podman.socket && systemctl start --user podman.socket
-podman system service -t 0 &
+sudo copy podman /usr/local/bin
+sudo systemctl --user enable --now podman.socket && sudo systemctl start --user podman.socket
+sudo podman system service -t 0 &
