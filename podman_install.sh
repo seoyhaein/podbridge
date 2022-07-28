296c75feb98e2a5014c4ea2858f33451d852bc07
seoyhaein <seoy@bio-caleb.com>
2022-07-28 22:33:49 +0900

    doc commit

--- podman_install.sh
+++ podman_install.sh
@@ -26,23 +26,30 @@ sudo apt-get install \
   uidmap
 
 # install runc
+pushd ~/gopath/src/github.com/seoyhaein/
 git clone -b v1.1.3 https://github.com/opencontainers/runc.git && pushd runc
 make
 cp runc /usr/local/bin
 popd
+popd
+
 # install conmon
+pushd ~/gopath/src/github.com/seoyhaein/
 git clone -b v2.1.3 https://github.com/containers/conmon.git && pushd conmon
 make
 pushd bin/
 cp conmon /usr/local/bin
 popd
 popd
+popd
 
 # install podman
+pushd ~/gopath/src/github.com/seoyhaein/
 git clone -b v4.1.0 https://github.com/containers/podman.git && pushd podman
 make binaries
 pushd bin/
 sudo cp podman /usr/local/bin
+popd
 popd
 popd
 sudo systemctl --user enable --now podman.socket && sudo systemctl start --user podman.socket
