36a995c6abd75da2c05ad03b86e2a4ad0b2cb33b
seoyhaein <seoy@bio-caleb.com>
2022-07-28 20:18:00 +0900

    doc commit

---
+++ podman_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+
+git clone -b v4.1.0 https://github.com/containers/podman.git
+make binaries
+cd bin/
+copy podman /usr/local/bin
+systemctl --user enable --now podman.socket && systemctl start --user podman.socket
+podman system service -t 0 &
