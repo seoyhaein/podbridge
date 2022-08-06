1a7a2d748507e9e9266c168d4b4a522f4195d070
seoyhaein <seoy@bio-caleb.com>
2022-08-06 21:04:55 +0900

    doc commit

--- localmachine/container.go
+++ localmachine/container.go
@@ -14,7 +14,7 @@ import (
 // 아래와 같은 방식으로 제작한다. 다만 리턴에 대한 문제는 생가해봐야 한다.
 
 // 중요!! 지속적으로 container 의 status 를 확인해줘야 함으로, goroutine, 루프 구문이 들어가고 context 가 들어가고, channel 이 들어가야 할듯 하다.
-// 퍼포먼스 문제가 있을까?? 일단 고민좀 해보자.
+// 퍼포먼스 문제가 있을까?? 일단 고민좀 해보자. 여러 컨테이너를 지속적으로 해야 함으로 이런 방식은 문제가 있을듯 하다. 일단 좀더 고민해 보자.
 
 func ContainerExists(id string) error {
 	cmd := exec.Command("podman", "container", "exists", id)
